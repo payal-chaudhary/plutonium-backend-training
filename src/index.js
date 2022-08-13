622e775f4763478b2a7c1e5de26b566120a97b84
payal-chaudhary <payalsiwach96@gmail.com>
2022-08-13 09:50:01 +0530

    see

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {default:mongoose} = require('mongoose')
 const route = require('./routes/route.js');
 const app = express();
 
 app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://payal-chaudhary:Love9711!!@cluster0.jjm7nst.mongodb.net/?retryWrites=true&w=majority")
+.then( ()=> console.log("Mongodb is connected"))
+.catch(  err => console.log(err))
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
