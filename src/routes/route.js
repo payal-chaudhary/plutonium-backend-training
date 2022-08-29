fe6c65ad128e0a67bc223aec286cd1b2e1544818
payal-chaudhary <payalsiwach96@gmail.com>
2022-08-29 21:47:25 +0530

    new

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,26 +1,16 @@
 const express = require('express');
 const router = express.Router();
-//const userController= require("../controllers/userController")
 const user= require("../controllers/newUserController")
 const middle= require("../middleware/auth")
-//const jwt= require("jsonwebtoken")
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-//router.post("/users", userController.createUser  )
-
-
-//router.post("/login", userController.loginUser)
-
-//The userId is sent by front end
+///assignment///
 router.get("/users/:userId", middle.statusCheck, user.getUserData)
 
 router.put("/users/:userId", middle.statusCheck, user.updateUser)
 router.delete("/users/:userId", middle.statusCheck, user.deleteApi)
 
 
-///assignment///
+
 router.post("/createNewUser", user.createNewUser)
 router.post("/newUserlogin",user.newUserlogin)
 
