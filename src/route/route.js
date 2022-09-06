0488f027e5ea450f5f682cac22e9c7acb4a741bd
payal-chaudhary <payalsiwach96@gmail.com>
2022-09-06 12:44:23 +0530

    deletblogby param

--- src/route/route.js
+++ src/route/route.js
@@ -1,12 +1,14 @@
 const express =require("express");
 const router = express.Router();
 const createAuthor = require("../controller/authorController")
-const {createBlog, blogsDetails} = require('../controller/blogController')
+const {createBlog, blogsDetails, deleteBlogByParams} = require('../controller/blogController')
 
 
-router.post("/authors", createAuthor )
-router.post("/blogs", createBlog )
-router.get("/getBlogs", blogsDetails )
+
+router.post("/authors", createAuthor)
+router.post("/blogs", createBlog)
+router.get("/getBlogs", blogsDetails)
+router.delete("/blogs/:blogId", deleteBlogByParams)
 
 
 module.exports= router;
