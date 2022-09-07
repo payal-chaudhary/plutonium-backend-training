ad3e610a82c784923702b671d1590196130737eb
Vikas Yadav <vikasyadav14nov@gmail.com>
2022-09-07 16:29:40 +0530

    auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,34 +1,30 @@
+const jwt = require("jsonwebtoken");
 
+//////Authenticate//////
 
+const authenticate = function (req, res, next) {
+  let token = req.headers[`x-api-key`]
 
+  if (!token) return res.status(404).send({ status: false, msg: "token must be present" });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  decodedToken = jwt.verify(token, "project-blog team 67")
+  if (decodedToken) {
+    next();
+  } else {
+    return res.status(400).send({ status: false, message: 'Token is not valid' })
+  }
+}
 
 
 /////Authorization/////
 
-const authorise = function(req, res, next) {
+const authorise = function (req, res, next) {
 
-let requesteduserId = req.params.userId
-if(requesteduserId!==req.loggedInUser){
-  return res.send({status:false, msg:"permission denied"})
+  let requesteduserId = req.params.userId
+  if (requesteduserId !== req.decodedToken.userId) {
+    return res.status(401).send({ status: false, msg: "permission denied" })
+  }
+  next()
 }
-next()
-}
+
+module.exports = { authenticate, authorise }
--- src/route/route.js
+++ src/route/route.js
@@ -1,16 +1,16 @@
-const express =require("express");
+const express = require("express");
 const router = express.Router();
 const createAuthor = require("../controller/authorController")
-const {createBlog, blogsDetails, deleteBlogByParams, deleteBlogByQuery, updateBlog} = require('../controller/blogController')
-
+const { createBlog, blogsDetails, deleteBlogByParams, deleteBlogByQuery, updateBlog } = require('../controller/blogController')
+const { authenticate, authorise } = require("../middleware/auth")
 
 
 router.post("/authors", createAuthor)
 router.post("/blogs", createBlog)
-router.get("/blogs", blogsDetails)
-router.put("/blogs/:blogId", updateBlog)
-router.delete("/blogs/:blogId", deleteBlogByParams)
-router.delete("/blogs", deleteBlogByQuery)
+router.get("/blogs", authenticate, authorise, blogsDetails)
+router.put("/blogs/:blogId", authenticate, authorise, updateBlog)
+router.delete("/blogs/:blogId", authenticate, authorise, deleteBlogByParams)
+router.delete("/blogs", authenticate, authorise, deleteBlogByQuery)
 
 
-module.exports= router;
+module.exports = router;
