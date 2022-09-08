b0a1ce052ba7ad92427511724db4eb8079743542
payal-chaudhary <payalsiwach96@gmail.com>
2022-09-08 13:11:15 +0530

    day3 changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,37 +1,50 @@
 const jwt = require("jsonwebtoken");
-
+const blogModel = require("../model/blogModel");
 //////Authenticate//////
 
 const authenticate = function (req, res, next) {
   try {
-    let token = req.headers[`x-api-key`]
+    let token = req.headers[`x-api-key`];
 
-    if (!token) return res.status(404).send({ status: false, msg: "token must be present" });
+    if (!token)
+      return res
+        .status(404)
+        .send({ status: false, msg: "token must be present" });
 
-    let decodedToken = jwt.verify(token, "project-blog team 67", function(err, decode){
-      if(err){
-        return res.send("invalid")
+    let decodedToken = jwt.verify(
+      token,
+      "project-blog team 67",
+      function (err, decode) {
+        if (err) {
+          return res.send("invalid");
+        }
+        return decode;
       }
-      return decode
-    })
-    req.token = decodedToken
+    );
+    req.token = decodedToken;
     next();
   } catch (error) {
-    return res.status(500).send({ status: false, msg: error.message })
+    return res.status(500).send({ status: false, msg: error.message });
   }
-
-}
-
+};
 
 /////Authorization/////
 
-const authorise = function (req, res, next) {
-
-  let requestedId = req.query.authorId
-  if (requestedId !== req.token.authorId) {
-    return res.status(401).send({ status: false, msg: "permission denied" })
+const authorise = async function (req, res, next) {
+  
+  let getBlogByParam = req.params.blogId;
+  if (!getBlogByParam) {
+    let getBlogByQuery = req.query;
+    if (getBlogByQuery.authorId == req.token.authorId) {
+      next();
+    }if(!getBlogByParam)
+    getBlogByQuery.authorId = req.token.authorId;
+    let getBlogId = await blogModel.find(getBlogByQuery);
+    if (!getBlogId) {
+      return res.status(401).send({ status: false, msg: "permission denied" });
+    }
+    next();
   }
-  next()
-}
+};
 
-module.exports = { authenticate, authorise }
+module.exports = { authenticate, authorise };
